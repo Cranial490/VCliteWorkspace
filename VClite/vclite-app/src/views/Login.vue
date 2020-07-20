38ba38813a0357a3297bfdc287b5e5ef8d3c658f
PP067807 <Pranjal.Paliwal@cerner.com>
2020-07-20 17:42:36 +0530

    minor change

--- VClite/vclite-app/src/views/Login.vue
+++ VClite/vclite-app/src/views/Login.vue
@@ -1,15 +1,14 @@
 <template>
     <b-container class="loginBox">
-        <h2>Login</h2>
     <div class="loginContent shadow">
         <form @submit.prevent="handleSubmit">
             <div class="form-group">
-                <label for="username">Username</label>
+                <label for="username" style="float:left;font-weight:bold;">Username</label>
                 <input type="text" v-model="username" name="username" class="form-control" :class="{ 'is-invalid': submitted && !username }" />
                 <div v-show="submitted && !username" class="invalid-feedback">Username is required</div>
             </div>
             <div class="form-group">
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password" style="float:left;font-weight:bold;">Password</label>
                 <input type="password" v-model="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && !password }" />
                 <div v-show="submitted && !password" class="invalid-feedback">Password is required</div>
             </div>
